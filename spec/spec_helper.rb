4863eaa5f272a2745883f09882894db56e0052eb
Acezon Cay <hello@acezoncay.me>
2017-11-06 19:35:05 +1100

    Removed unnecessary Rack::Test::Methods invocation in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ $: << File.expand_path('../../', __FILE__)
 ENV["RACK_ENV"] = "test"
 
 RSpec.configure do |config|
-  config.include Rack::Test::Methods
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
